bf6dcc1fe5d41dbe50e12a0d6e5dc5f631ab16b2
najiaolong <394975750@qq.com>
2022-06-28 11:05:46 +0800

    初始版本

---
+++ Lib/InfinovoBLESDK.framework/Headers/InfinovoBLESDK.h
@@ -0,0 +1,18 @@
+//
+//  InfinovoBLESDK.h
+//  InfinovoBLESDK
+//
+//  Created by 九诺医疗 on 4/14/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InfinovoBLESDK.
+FOUNDATION_EXPORT double InfinovoBLESDKVersionNumber;
+
+//! Project version string for InfinovoBLESDK.
+FOUNDATION_EXPORT const unsigned char InfinovoBLESDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfinovoBLESDK/PublicHeader.h>
+
+
